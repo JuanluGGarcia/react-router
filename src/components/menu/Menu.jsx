200b2bb213444d9f680303396282139b2ad024c2
Juan Luis Garcia Garcia <juanluisggarcia@gmail.com>
2024-01-28 22:37:12 +0100

    explicacion menu

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,5 +1,18 @@
 import { Link } from "react-router-dom";
 
+
+/* Para la practica de los planetas, Adri nos dice que todo lo que se repita que lo metamos en un array de objetos. Por ejemplo el menu pues podria ser: 
+
+    <ul>
+        {menu.map(menu => (
+            <li key={menu.id}>
+            <Link to={menu.path}>
+                {menu.text}
+            </Link>
+        ))}
+    </ul>
+*/
+
 const Menu = () => {
     return (
         <nav>
